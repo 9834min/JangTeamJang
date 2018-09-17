07883134832dabb07abbf29e2d5bde8faa1a15f0
sunjbs <sunjbs98@gmail.com>
2018-09-17 21:36:13 +0900

    Setting Tensorflow Done

--- app/src/main/java/com/nextop/nextopocr/MainActivity.java
+++ app/src/main/java/com/nextop/nextopocr/MainActivity.java
@@ -10,21 +10,26 @@ import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
 
 public class MainActivity extends AppCompatActivity {
-
+    static{
+        System.loadLibrary("tensorflow_inference");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         TextView tvHello = (TextView) findViewById(R.id.helloText);
+
+        //NativeLibrary.load();
         try (Graph g = new Graph()) {
             final String value = "Hello from " + TensorFlow.version();
-            tvHello.setText( value );
+            tvHello.setText(value);
 
             // Construct the computation graph with a single operation, a constant
             // named "MyConst" with a value "value".
             try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
                 // The Java API doesn't yet include convenience functions for adding operations.
                 g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.err.print(e);
             }
 
@@ -33,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
                  // Generally, there may be multiple output tensors, all of them must be closed to prevent resource leaks.
                  Tensor output = s.runner().fetch("MyConst").run().get(0)) {
                 System.out.println(new String(output.bytesValue(), "UTF-8"));
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.err.print(e);
             }
         }
-        setContentView(R.layout.activity_main);
     }
 }
