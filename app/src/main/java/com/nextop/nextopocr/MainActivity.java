d111103f3d0720f8b6cf38cd11524b3c6189976e
9834min <9834min@gmail.com>
2018-09-19 20:59:30 +0900

    dd

--- app/src/main/java/com/nextop/nextopocr/MainActivity.java
+++ app/src/main/java/com/nextop/nextopocr/MainActivity.java
@@ -1,7 +1,12 @@
 package com.nextop.nextopocr;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.tensorflow.Graph;
@@ -10,37 +15,31 @@ import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
 
 public class MainActivity extends AppCompatActivity {
-    static{
-        System.loadLibrary("tensorflow_inference");
-    }
+
+    Button btn = null;
+    ImageView iv = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvHello = (TextView) findViewById(R.id.helloText);
-
-        //NativeLibrary.load();
-        try (Graph g = new Graph()) {
-            final String value = "Hello from " + TensorFlow.version();
-            tvHello.setText(value);
-
-            // Construct the computation graph with a single operation, a constant
-            // named "MyConst" with a value "value".
-            try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
-                // The Java API doesn't yet include convenience functions for adding operations.
-                g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
-            } catch (Exception e) {
-                System.err.print(e);
+        setup();
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
             }
-
-            // Execute the "MyConst" operation in a Session.
-            try (Session s = new Session(g);
-                 // Generally, there may be multiple output tensors, all of them must be closed to prevent resource leaks.
-                 Tensor output = s.runner().fetch("MyConst").run().get(0)) {
-                System.out.println(new String(output.bytesValue(), "UTF-8"));
-            } catch (Exception e) {
-                System.err.print(e);
-            }
-        }
+        });
     }
+
+    private void setup()
+    {
+        btn = (Button)findViewById(R.id.btn);
+        iv = (ImageView)findViewById(R.id.iv);
+    }
+
 }
